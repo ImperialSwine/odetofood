9cece76706bb894a20805d147ed3ad07a9fa2ac7
Sachin Krishnamoorthy <s.krishnamoorthy@wisr.com.au>
2020-05-28 21:04:49 +1000

    Restaurant Interface and Data made

---
+++ odetofood/OdeToFood.Core/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Core
+{
+    public enum CuisineType
+    {
+        None,
+        Mexican,
+        Chinese,
+        Italian,
+        Indian
+    }
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
